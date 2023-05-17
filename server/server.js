feaa10853ad6af6b2cb8acfb6184972924e151b4
Andreas Copan <avcopan@gmail.com>
2023-05-16 21:43:26 -0400

    Sets up a simple starter

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const PORT = 8000;
+const app = express();
+
+app.use(express.static("server/public/"));
+app.use(express.json());
+
+app.listen(PORT, () => {});
