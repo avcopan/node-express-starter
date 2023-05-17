902e5dd4487647ff03f7ff5c3899a9312ab09e5a
Andreas Copan <avcopan@gmail.com>
2023-05-16 21:47:31 -0400

    Class notes

--- server/public/client.js
+++ server/public/client.js
@@ -1,1 +1,47 @@
 console.log('script sourced.')
+
+function refreshUsers() {
+  fetch('/users')
+    .then(res => res.json())
+    .then(console.log);
+}
+
+fetch('/users/0')
+  .then(res => res.json())
+  .then(console.log);
+
+let newUser = {
+  name: 'Charlie',
+  role: 'super-admin',
+}
+
+fetch('/users', {
+  method: 'POST',
+  body: JSON.stringify(newUser),
+  headers: {
+    'Content-Type': 'application/json',
+  }
+})
+  .then(() => refreshUsers());
+
+let updatedUser = {
+  name: 'Anne',
+  role: 'user',
+}
+
+fetch('/users/0', {
+  method: 'PUT',
+  body: JSON.stringify(updatedUser),
+  headers: {
+    'Content-Type': 'application/json',
+  }
+})
+  .then(() => refreshUsers());
+
+fetch('/users/1', {
+  method: 'DELETE',
+})
+  .then(res => res.json())
+  .then((deletedUser) => {
+    console.log(deletedUser)
+  });
--- server/server.js
+++ server/server.js
@@ -3,7 +3,46 @@ const express = require("express");
 const PORT = 8000;
 const app = express();
 
+let users = [
+  {
+    name: "Anne",
+    role: "admin",
+  },
+  {
+    name: "Bob",
+    role: "user",
+  },
+];
+
 app.use(express.static("server/public/"));
 app.use(express.json());
+
+app.get("/users", (req, res) => {
+  res.status(200).send(users);
+});
+
+app.get("/users/:userIndex", (req, res) => {
+  let userIndex = req.params.userIndex;
+  console.log("Sending user:", users[userIndex]);
+  res.status(200).send(users[userIndex]);
+});
+
+app.post("/users", (req, res) => {
+  users.push(req.body);
+  res.sendStatus(201);
+});
+
+app.put("/users/:userIndex", (req, res) => {
+  let updatedUser = req.body;
+  let userIndex = req.params.userIndex;
+  users[userIndex] = updatedUser;
+});
+
+app.delete("/users/:userIndex", (req, res) => {
+  let userIndex = req.params.userIndex;
+  let deletedUser = users[userIndex];
+  users[userIndex] = undefined;
+  res.status(200).send(deletedUser);
+});
 
 app.listen(PORT, () => {});
