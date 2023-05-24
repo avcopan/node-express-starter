3c02a6209cbc6ddfaed2d293b46dec11f3517db8
Andreas Copan <avcopan@gmail.com>
2023-05-24 19:01:42 -0400

    notes 1

--- server/server.js
+++ server/server.js
@@ -1,9 +1,32 @@
 const express = require("express");
+const pg = require("pg");
 
 const PORT = process.env.PORT || 8000;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 const app = express();
+const pool = new pg.Pool({
+  host: "db.bit.io",
+  port: 5432,
+  ssl: true,
+  database: "avcopan/trial",
+  user: "songs",
+  password: DB_PASSWORD,
+});
 
 app.use(express.static("server/public/"));
 app.use(express.json());
 
-app.listen(PORT, () => {});
+app.get("/songs", (req, res) => {
+  pool.query('SELECT * FROM "songs"')
+    .then((result) => {
+      res.status(200).send(result.rows());
+    })
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log("App is listening on port", PORT);
+});
